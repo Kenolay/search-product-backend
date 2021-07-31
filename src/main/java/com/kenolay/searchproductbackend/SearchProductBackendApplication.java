8ff84408286c8773e2b06bff111f810e16524ac8
Kenolay <salmin-1995@mail.ru>
2021-07-31 19:47:41 +0400

    Initial commit

---
+++ src/main/java/com/kenolay/searchproductbackend/SearchProductBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kenolay.searchproductbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchProductBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchProductBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kenolay/searchproductbackend/SearchProductBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenolay.searchproductbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchProductBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
